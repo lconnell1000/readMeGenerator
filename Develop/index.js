7c4c2e7431d33ec5a50837c8a1a9afbb25dc3b2a
lconnell1000 <lconnell1000@gmail.com>
2022-02-09 12:33:13 +1030

    second commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,23 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+.prompt([
+    {
+      type: 'input',
+      name: 'Title',
+      message: 'What is the title for your project?',
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'What is your location',
+      },
+      {
+        type: 'input',
+        name: 'bio',
+        message: 'Please enter your bio',
+      },
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
