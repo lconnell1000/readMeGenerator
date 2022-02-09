f1a8f5d2e2528e2398cb0e821410c2f76ec997d0
lconnell1000 <lconnell1000@gmail.com>
2022-02-09 13:05:32 +1030

    just need to add readme template

--- Develop/index.js
+++ Develop/index.js
@@ -2,28 +2,75 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-.prompt([
+inquirer.prompt([
     {
       type: 'input',
-      name: 'Title',
+      name: 'title',
       message: 'What is the title for your project?',
     },
     {
         type: 'input',
-        name: 'location',
-        message: 'What is your location',
+        name: 'decription',
+        message: 'Please enter a description for your project',
       },
       {
         type: 'input',
-        name: 'bio',
-        message: 'Please enter your bio',
+        name: 'installation',
+        message: 'Please enter installation instructions for your project',
       },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter usage information for your project ',
+      },
+      {
+        type: 'rawlist',
+        name: 'license',
+        message: 'Please select which license you are using for your project ',
+        choices: ["GNU 2", "GNU 3", "MIT", "Apache License 2"],
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "Please enter contribution guidelines for your project",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Please enter any test instructions for your project ",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "Please enter your Github username ",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address ",
+      },
+    ])
+.then((data) => {
+    if (data.license === "GNU 2") {
+        var badge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+    }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    else if (data.license === "GNU 3") {
+        var badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    }
 
-// TODO: Create a function to initialize app
-function init() {}
+    else if (data.license === "MIT") {
+        var badge =  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    }
 
-// Function call to initialize app
-init();
+    else {
+        var badge =  "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    }
+    const readMe = `
+    # ${data.title}
+
+    ${badge}
+
+
+    `
+}
